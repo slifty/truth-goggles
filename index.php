05fa731aea3ad3e96b2af46782e52ea4d6093183
Daniel Schultz <slifty@gmail.com>
2011-07-26 03:44:04 -0400

    added some more info on demo

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 		<div id="container">
 			<div id="header"></div>
 			<div id="main">
-				<a href="javascript:var%20s%20=%20document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='http://critical.istheinternetabigtruck.com/critical.min.js';void(0);">Track the Truth</a>
+				<a href="javascript:var%20s%20=%20document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='http://critical.istheinternetabigtruck.com/critical.min.js';void(0);">Drag me to your toolbar to Track the Truth</a>
 				<p>While advising his Fox News viewers to talk about inflation at their Thanksgiving dinners, Glenn Beck falsely claimed that the government removed food and energy prices from <b>its measure <i>of</i> inflation</b> to hide rising prices, that a survey showed economists are “worried” about inflation, and that Social Security recipients are not receiving a cost-of-living adjustment because the government “changed the calculation.”</p>
 				<br />
 				
