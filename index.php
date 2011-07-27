a84e40816d75ad17b48f2a0027f2c49e54244dd1
Daniel Schultz <slifty@gmail.com>
2011-07-27 15:50:33 -0400

    added some more external examples

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
 		<div id="container">
 			<div id="header"></div>
 			<div id="main">
-				<a href="javascript:var%20criticalDomain='http://<?PHP echo($SITE_DOMAIN.$SITE_ROOT); ?>';var%20s=document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src=criticalDomain+'/critical.min.js';void(0);">Drag me to your toolbar to Track the Truth</a>
+				<p><a href="javascript:var%20criticalDomain='http://<?PHP echo($SITE_DOMAIN.$SITE_ROOT); ?>';var%20s=document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src=criticalDomain+'/critical.min.js';void(0);">Apply Truth Goggles</a> <-- Drag this to your toolbar!</p>
+				<br />
 				<p>While advising his Fox News viewers to talk about inflation at their Thanksgiving dinners, Glenn Beck falsely claimed that the government removed food and energy prices from <b>its measure <i>of</i> inflation</b> to hide rising prices, that a survey showed economists are “worried” about inflation, and that Social Security recipients are not receiving a cost-of-living adjustment because the government “changed the calculation.”</p>
 				<br />
-				
 				<p>
 					<span class="critical false">False statements appear red.</span><br /><br />
 					<span class="critical mostlyFalse">Mostly false statements appear yellow.</span><br /><br />
@@ -22,7 +22,13 @@
 					<span class="critical true">True statements appear green.</span><br /><br />
 				</p>
 				
-				<p>It also works elsewhere on the internet!  Check out <a href="http://kaystreet.wordpress.com/2010/11/27/">this article</a> and try running the bookmarklet again.</p>
+				<p>It also works elsewhere on the internet!  Check out any of these articles:</p>
+				<ul>
+					<li><a href="http://kaystreet.wordpress.com/2010/11/27" target="_blank">Example 1</a></li>
+					<li><a href="http://mediamatters.org/mobile/research/201011230057" target="_blank">Example 2</a></li>
+					<li><a href="http://www.commynews.com/tag/dinner" target="_blank">Example 3</a></li>
+				</ul>
+				<p>and try running the bookmarklet again.</p>
 			</div>
 			<div id="footer"></div>
 		</div>
